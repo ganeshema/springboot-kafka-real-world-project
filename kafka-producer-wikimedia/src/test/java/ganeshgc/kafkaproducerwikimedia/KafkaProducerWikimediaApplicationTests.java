1bb9bc6caac0eaec6d2a294e583725b7f75e9037
Ganesh GC <ganeshlema2021@gmail.com>
2024-06-30 08:49:41 -0500

    initial commit after creation of spring boot project and new module

---
+++ kafka-producer-wikimedia/src/test/java/ganeshgc/kafkaproducerwikimedia/KafkaProducerWikimediaApplicationTests.java
@@ -0,0 +1,13 @@
+package ganeshgc.kafkaproducerwikimedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerWikimediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
